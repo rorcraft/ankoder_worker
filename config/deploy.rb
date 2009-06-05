3619ddeb3b022706ba92121c33f24303a388fd3e
rorcraft <rorcraft@rorcraft-desktop.(none)>
2009-06-05 15:08:34 +0800

    Updated deploy.rb for ankoder worker

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,3 +17,15 @@ namespace :app do
     run "cd #{deploy_to}/current/config && rm -f database.yml && ln -s #{shared_path}/svn_trunk/config/database.yml"
   end
 end         
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end 
+              
+  [:start, :stop].each do |t| 
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end 
+  end 
+end 
