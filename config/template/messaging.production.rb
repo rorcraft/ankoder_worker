1cea6cf3d11e4b43428ae9377f04f4fc10866749
rorcraft <rorcraft@rorcraft-desktop.(none)>
2009-06-08 19:11:24 +0800

    broker.production.yml and message.production.rb

---
+++ config/template/messaging.production.rb
@@ -0,0 +1,23 @@
+#
+# Add your destination definitions here
+# can also be used to configure filters, and processor groups
+#
+# http://code.google.com/p/activemessaging/wiki/Configuration
+ActiveMessaging::Gateway.define do |s|
+  #s.filter :some_filter, :only=>:orders
+  s.processor_group :transcoder, :transcoder_worker
+  s.processor_group :downloader, :downloader_worker
+    
+  # NB:
+  # reliable_msg "/queue/dev/Converter"
+  # sqs "_queue_dev_Converter"
+  
+  if ENV['RAILS_ENV'] != "production"
+    s.destination :transcode_worker, '_queue_dev_Converter_qsbwee'
+    s.destination :downloader_worker, '_queue_dev_Downloader_qsbwee'
+  else
+    s.destination :transcode_worker, '_queue_asqs_Converter'
+    s.destination :downloader_worker, '_queue_asqs_Downloader'  
+  end
+
+end
