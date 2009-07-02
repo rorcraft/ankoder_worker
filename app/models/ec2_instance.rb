6877e227c7a545db03f0fb04fc3f0f445b6eb476
caiyufei <neldorluothe@hotmail.com>
2009-07-02 17:17:46 +0800

    Changed ec2_instance primary key from id to escaped_hostname.

--- app/models/ec2_instance.rb
+++ app/models/ec2_instance.rb
@@ -3,4 +3,8 @@ class Ec2Instance < ActiveResource::Base
   RUNNING = "RUNNING"
   TERMINATED = "TERMINATED"
   self.site = AR_SITE
+
+  def escape_primary_key(string)
+    string.gsub(/\./,"*").gsub(/\//,"\\")
+  end
 end
