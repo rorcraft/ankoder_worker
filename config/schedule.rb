90b4ed897eb885bdcdf1e5a7176ea29ce451548f
caiyufei <neldorluothe@hotmail.com>
2009-07-28 17:27:57 +0800

    cosmetic change; now chron job can be configured in environments/*.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
 every 10.minutes do
-  command "#{File.join(File.dirname(__FILE__),"../script/maintain_file_system")}"
+  command File.join(File.dirname(__FILE__),"../script/maintain_file_system")
 end
