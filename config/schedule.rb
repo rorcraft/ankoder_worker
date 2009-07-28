b19d499407cdac820d53706a4da8625b00f23968
caiyufei <neldorluothe@hotmail.com>
2009-07-28 18:30:54 +0800

    deceptive path of schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
 every 10.minutes do
-  command File.expand_path(File.join(File.dirname(__FILE__),"../script/maintain_file_system"))
+  command File.expand_path(File.join(File.dirname(__FILE__),"/script/maintain_file_system"))
 end
