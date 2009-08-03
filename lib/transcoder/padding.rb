b16a772b881bc7d6851a675640ae1032ee7881f1
Alan C <alanchiang@rorcraft.com>
2009-08-03 17:54:52 +0800

    an attempt to fix the bug for video not generated according to profiles' dimensions when padding is needed

--- lib/transcoder/padding.rb
+++ lib/transcoder/padding.rb
@@ -3,43 +3,77 @@ module Transcoder
   module Padding
     
     # split padding into symmetric halves
-    def split_padding length
-      pad1 = pad2 = even_size(length)/2
-      return even_size(pad1), even_size(pad2) #padding must be even number themselves.
-    end 
+    # current: the width/height needed to be altered
+    def split_padding video_current, video_other, profile_current, profile_other
 
+      #profile_current = 2* ( after/2 )+ padding *2
+
+      ratio = video_current.to_f / video_other.to_f
+
+      after_f = after = profile_other * ratio
+
+      after = float_to_even_int after_f
+      after = 2 if after <= 0
+
+      if ((profile_current - after)/2).to_i.odd?
+        if after > after_f
+          after -= 2
+        else
+          after += 2
+        end
+      end
+      pad = (profile_current - after) / 2
+
+      # fallback if really really something awful occurs (which should not happen anyway)
+      pad = 0 if pad < 0
+      after = profile_current if after > profile_current
+
+      return pad, pad, after #padding must be even number themselves.
+    end
+
+    def float_to_even_int(float)
+      if float % 1 > 0.5000
+        result = float.ceil
+        result -= 1 if result.odd?
+      else
+        result = float.floor
+        result += 1 if result.odd?
+      end
+      result
+    end
     # turns odd numbers into even & even ones into themselves
-    def even_size(size)
+    def even_size(size, increase = true)
       size = (size.to_i rescue 240)
-      size += 1 if size.odd?
-      size 
+      size += 1 if size.odd? && increase
+      size -= 1 if size.odd? && (increase != true)
+      size
     end
 
     def padding(profile_width, profile_height, video_width, video_height)
       return {"result_width" => 320, "result_height" => 240} if profile_width.to_i <= 0 && video_width.to_i <= 0
       return {"result_width" => video_width, "result_height" => video_height} if profile_width.to_i <= 0 && profile_height.to_i <= 0
-      return {"result_width" => profile_width, "result_height" => profile_height} if video_width.to_i < 0 or video_height.to_i < 0      
-      return {"result_width" => profile_width, "result_height" => even_size(profile_width * video_height / video_width) } if profile_width.to_i > 0 && profile_height.to_i == 0      
-      return {"result_width" => even_size(profile_height * video_width / video_height), "result_height" => profile_height } if profile_width.to_i == 0 && profile_height.to_i > 0      
+      return {"result_width" => profile_width, "result_height" => profile_height} if video_width.to_i < 0 or video_height.to_i < 0
+      return {"result_width" => profile_width, "result_height" => even_size(profile_width * video_height / video_width) } if profile_width.to_i > 0 && profile_height.to_i == 0
+      return {"result_width" => even_size(profile_height * video_width / video_height), "result_height" => profile_height } if profile_width.to_i == 0 && profile_height.to_i > 0
       return {"result_width" => profile_width, "result_height" => profile_height } if video_width.to_f / video_height.to_f == profile_width.to_f / profile_height.to_f
 
       result = {}
       %w{profile_width profile_height video_width video_height}.each do |measure|
-        eval "#{measure} = #{measure}.to_f" 
+        eval "#{measure} = #{measure}.to_f"
       end
       ratio_width , ratio_height  = profile_width / video_width , profile_height / video_height
-      result_width, result_height = profile_width, profile_height 
+      result_width, result_height = profile_width, profile_height
 
-      ratio = even_size(profile_width).to_f / even_size(profile_height).to_f      
-      video_ratio = even_size(video_width).to_f / even_size(video_height).to_f      
+      ratio = even_size(profile_width).to_f / even_size(profile_height).to_f
+      video_ratio = even_size(video_width).to_f / even_size(video_height).to_f
 
       if (ratio > 1.555)
         result["aspect_ratio"] = "16:9"
-        ratio_width = 16 
+        ratio_width = 16
         ratio_height = 9
-      else 
+      else
         result["aspect_ratio"] = "4:3"
-        ratio_width = 4 
+        ratio_width = 4
         ratio_height = 3
       end
 
@@ -48,21 +82,19 @@ module Transcoder
       else
         # pad top and bottom; profile too high
         if profile_width*video_height < profile_height*video_width
-          result_height = even_size profile_width*video_height/video_width
-          result["padtop"], result["padbottom"] = split_padding(profile_height - result_height)
+          result["padtop"], result["padbottom"], result_height = split_padding(video_height, video_width, profile_height, profile_width)
         # pad left and right; profile too wide
         elsif profile_width*video_height > profile_height*video_width
-          result_width = even_size profile_height*video_width/video_height
-          result["padleft"], result["padright"] = split_padding(profile_width  - result_width) 
-        end                                    
+          result["padleft"], result["padright"], result_width = split_padding(video_width, video_height, profile_width, profile_height)
+        end
       end
-      
-      result["result_width"], result["result_height"] = even_size(result_width), even_size(result_height)
-      
-      return result 
+
+      result["result_width"], result["result_height"] = result_width, result_height
+
+      return result
     end
-    
-   
+
+
   end
 
 end
--- spec/transcoder/padding_spec.rb
+++ spec/transcoder/padding_spec.rb
@@ -1,4 +1,4 @@
-require 'active_support'
+#require 'active_support'
 require File.expand_path(File.dirname(__FILE__) + "/../../lib/transcoder/padding")
 
 describe Transcoder::Padding do
@@ -6,44 +6,63 @@ describe Transcoder::Padding do
   class TestPadding 
     include Transcoder::Padding    
   end
-  
+
   before :each do
     @p = TestPadding.new
   end
-  
+
   it "even_size should return round up if odd number" do
     @p.even_size(2).should == 2
     @p.even_size(3).should == 4
     @p.even_size(5).should == 6
   end
-  
-  it "split_padding should split a length into two with even lengths" do
-    # Since 1 pixel should not be noticable, we can do this
-    @p.split_padding(10).should == [6,6]
-    @p.split_padding(8).should == [4,4]
-    @p.split_padding(13).should == [8,8]    
+
+  it "float_to_even_int should make (positive) floats to the best even integer" do
+    @p.float_to_even_int(13.5).should == 14
+    @p.float_to_even_int(12.5).should == 12
+    @p.float_to_even_int(10.999999).should == 10
+    @p.float_to_even_int(0.999999).should == 0
   end
-  
+
+  it "split_padding should split padding according to the width and height of target and original" do
+    #height test
+    @p.split_padding(300, 600, 480, 640).should == [80,80,320]
+    @p.split_padding(214, 320, 144, 176).should == [14,14,116] # h263 bug test case
+
+    #width test
+    @p.split_padding(15, 13, 20, 14).should == [2,2,16]
+  end
+
   describe ".padding(profile_width,profile_height,video_width,video_height)" do
     it "defaults to 320x240" do
       result = @p.padding(0,0,-1,-1)
-      result["result_height"].should == 240 
-      result["result_width"].should == 320      
+      result["result_height"].should == 240
+      result["result_width"].should == 320
     end
-    
-    it "if video_height < 0 or video_width < 0, padding should return profile dimensions" do    
+
+    it "if video_height < 0 or video_width < 0, padding should return profile dimensions" do
       result = @p.padding(320,240,-1,-1)
       result["result_height"].should == 240
       result["result_width"].should == 320
     end
-    
-    it "if profile dimensions = 0x0 then defaults to video original dimensions" do    
+
+    it "if profile dimensions = 0x0 then defaults to video original dimensions" do
       result = @p.padding(0,0,320,240)
       result["result_height"].should == 240
       result["result_width"].should == 320
     end
 
-    it "if profile = 16:9, video = 4:3 it'll pad width" do    
+    it "if profile dimensions = video dimensions it should return the same dimensions" do
+      result = @p.padding(320,240,320,240)
+      result["result_height"].should == 240
+      result["result_width"].should == 320
+      result["padtop"].to_i.should == 0
+      result["padbottom"].to_i.should == 0
+      result["padleft"].to_i.should == 0
+      result["padright"].to_i.should == 0
+    end
+
+    it "if profile = 16:9, video = 4:3 it'll pad width" do
       result = @p.padding(1600,900,400,300)
       result["result_height"].should == 900
       result["result_width"].should == 1200
@@ -52,30 +71,67 @@ describe Transcoder::Padding do
       result["padleft"].to_i.should == 200
       result["padright"].to_i.should == 200
     end
-    
+
+    it "if profile = 4:3, video = 16:9 it'll pad height" do
+      result = @p.padding(640,480,1920,1080)
+      result["result_height"].should == 360
+      result["result_width"].should == 640
+      result["padtop"].to_i.should == 60
+      result["padbottom"].to_i.should == 60
+      result["padleft"].to_i.should == 0
+      result["padright"].to_i.should == 0
+    end
+
     it "should return correct aspect ratio" do
       result = @p.padding(1600,900,400,300)
-      result["aspect_ratio"].should == "16:9"  
+      result["aspect_ratio"].should == "16:9"
       result = @p.padding(400,300,1600,900)
-      result["aspect_ratio"].should == "4:3"  
+      result["aspect_ratio"].should == "4:3"
     end
 
     it "if resize 640x480 to 320x240, it should return correct width and height" do
       result = @p.padding(320,240,640,480)
-      result["result_height"].should == 240 
+      result["result_height"].should == 240
       result["result_width"].should == 320
+    end
+
+    it "profile width = 2 and profile height = 2 should give sensible results" do
+      result = @p.padding(2,2,640,480)
+      result["result_height"].should == 2
+      result["result_width"].should == 2
     end
 
     it "if resize 688x368 to 320x240, it should return correct width and height" do
       result = @p.padding(320,240,688,368)
-      result["result_height"].should == 172 
+      result["result_height"].should == 172
       result["result_width"].should == 320
-      result["aspect_ratio"].should == "4:3"  
-      result["padtop"].to_i.should == 34 
-      result["padbottom"].to_i.should == 34 
+      result["aspect_ratio"].should == "4:3"
+      result["padtop"].to_i.should == 34
+      result["padbottom"].to_i.should == 34
+    end
+
+    it "should correct odd width and height to something even" do
+      result = @p.padding(320,240,687,367)
+      result["result_width"].should == 320
+      result["aspect_ratio"].should == "4:3"
+      (result["result_height"]+result["padtop"].to_i+result["padbottom"].to_i).should == 240
+    end
+
+    it "should handle near-to-zero width and heights" do
+      result1 = @p.padding(320,240,1,2)
+      result1["result_height"].should == 240
+      result2 = @p.padding(320,240,2,1)
+      result2["result_width"].should == 320
+    end
+
+    it "should return standard CIF files for h263" do # h263 bug test case
+      result = @p.padding(176, 144, 320, 214)
+      result["result_height"].should == 116
+      result["result_width"].should == 176
+      (result["result_height"]+result["padtop"].to_i+result["padbottom"].to_i).should == 144
     end
     # TODO: need to add more test cases for padding
-    
+
   end
 
 end
