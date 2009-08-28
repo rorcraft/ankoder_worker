6766a3c32b9b05290ce5118d6009962fb15a85dd
caiyufei <neldorluothe@hotmail.com>
2009-08-28 16:10:20 +0800

    install segmenter & ffmpeg-with-vhook

--- config/template/localproduction.rb
+++ config/template/localproduction.rb
@@ -9,24 +9,6 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-# See everything in the log (default is :info)
-# config.log_level = :debug
-
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
-
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
-
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host = "http://assets.example.com"
-
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
-
-# Enable threaded mode
-# config.threadsafe!
-#
 AR_SITE = 'http://workflow:r0rcr4ft@ar.localankoder.com'
 FFMPEG_PATH     = "/usr/local/bin/ffmpeg"
 FFMPEG2THEORA_PATH     = "/usr/local/bin/ffmpeg2theora"
@@ -36,7 +18,10 @@ CURL            = "/usr/bin/curl"
 FILE_FOLDER   = "/var/www/api_ankoder/current/file_system"
 PUBLIC_FOLDER = "/var/www/api_ankoder/current/public"
 THUMBNAIL_FOLDER = "/thumbnail"
+
 SEGMENTER_PATH = "/usr/local/bin/commit.segfault"
+VHOOK_WATERMARK_PATH = "/home/softie/ffmpegvhook/vhook/watermark.so"
+FFMPEG_WITH_VHOOK_PATH = "/home/softie/ffmpegvhook/ffmpeg"
 
 S3_ON         = false
 S3BUCKET = S3_BUCKET = "devbucket.ankoder.com"
--- config/template/production.rb
+++ config/template/production.rb
@@ -9,24 +9,6 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-# See everything in the log (default is :info)
-# config.log_level = :debug
-
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
-
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
-
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host = "http://assets.example.com"
-
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
-
-# Enable threaded mode
-# config.threadsafe!
-#
 AR_SITE = 'http://workflow:r0rcr4ft@ar.ankoder.com'
 FFMPEG_PATH     = "/usr/local/bin/ffmpeg"
 AXEL_PATH = "/usr/local/bin/axel"
@@ -36,7 +18,10 @@ CURL            = "/usr/bin/curl"
 FILE_FOLDER   = "/var/www/ankoderworker/current/file_system"
 PUBLIC_FOLDER = "/var/www/ankoderworker/current/public"
 THUMBNAIL_FOLDER = "/thumbnail"
+
 SEGMENTER_PATH = "/usr/local/bin/commit.segfault"
+VHOOK_WATERMARK_PATH = "/home/railsdeploy/ffmpegvhook/vhook/watermark.so"
+FFMPEG_WITH_VHOOK_PATH = "/home/railsdeploy/ffmpegvhook/ffmpeg"
 
 S3_ON         = true
 S3BUCKET = S3_BUCKET = "download.ankoder.com"
