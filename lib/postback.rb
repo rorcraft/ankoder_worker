800880a8999aea95b0b95807073c01e70920cf0a
isaiah <isaiah@isaiah-macpro.(none)>
2009-09-17 13:15:15 +0800

    postback message for 'Upload' modified. includes all relevant infomation.

--- lib/postback.rb
+++ lib/postback.rb
@@ -35,7 +35,7 @@ class Postback
         'error'   =>  error,
         'type'    => 'Convert',
         'Job'     =>  model.id,
-        'original_video_id'     => model.original_file.id,
+        'original_video_id' => model.original_file.id,
         'is_trimmed' => {true => true, nil => false, false => false}[((model.profile.trim_begin || model.profile.trim_end) && (model.convert_file.duration == model.original_file.duration))],
       }
       if result   == 'success'
@@ -61,14 +61,15 @@ class Postback
     when 'upload'
       # model = instance of Job
       message = {
-        'Job'     => model.id,
+        #'Job'     => model.id,
+        'job'     => upload_message(model),
         'result'  => result,
         'error'   => error,
         'type'    => 'Upload',
-        'Video'   => model.convert_file.id,
-        'url'     => model.get_upload_url,
-        'filename'=> model.convert_file.filename,
-        "thumbnail_filenames" => model.convert_file.thumbnails.map(&:filename)
+        #'Video'   => video,
+        #'Origin'  => model.original_file.attributes,
+        #'url'     => model.get_upload_url,
+        #"thumbnail_filenames" => model.convert_file.thumbnails.map(&:filename)
     }
     end
 
@@ -90,5 +91,17 @@ class Postback
     logger.error e.backtrace.to_yaml
   ensure
     return result
+  end
+
+  # Construct postback message for upload phase
+  # Input: instance of Job
+  # Output: job with original_file and convert file details
+  def self.upload_message(job)
+    input = job.original_file
+    output = job.convert_file
+    convert_file = {"name" => output.name, "duration" => output.duration, "size" => output.size, "id" => output.id, "filename" => output.s3_name, "url" => "#{job.get_upload_url}/#{output.s3_name}", "height" => output.height, "width" => output.width, "thumbnails" => output.thumbnails.map(&:filename) }
+    original_file = {"name" => input.name, "duration" => input.duration, "size" => input.size, "id" => input.id, "height" => input.height, "width" => input.width}
+    job_hash = {"id" => job.id, "profile" => {"name" => job.profile.name, "id" => job.profile.id }, "status" => job.status, "input_file" => original_file, "output_file" => convert_file}
+    job_hash
   end
 end
